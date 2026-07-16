0016bd8148ade7099d7bf076804a6dfa12c5b102
Phillipe Augusto <phillipe.a.a.mendonca@gmail.com>
2026-07-16 04:59:14 -0300

    Add string utility helpers for trimming whitespace
    Signed-off-by: Phillipe Augusto <phillipe.a.a.mendonca@gmai.com>

---
+++ utils/strings.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# String utility helpers
+# Safe to source from other scripts. Provides simple string functions
+# used across lib/ modules.
+
+# Trim leading and trailing whitespace from a string argument.
+# Usage: trimmed=$(string_trim "  value  ")
+string_trim() {
+    local s="$1"
+    printf '%s' "$s" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//'
+}
